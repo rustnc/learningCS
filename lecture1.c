7c537f33625225578e111e4c0142a3916cbcdf32
scooby <oscar20062023@outlook.com>
2023-09-02 22:09:58 -0500

    testing again

--- lecture1.c
+++ lecture1.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
+
 int main (void)
 {
-    printf("hello, world!\n");
+    string name = get_string(" what is your name? ");
+    printf("hello, %s!\n", name);
 }
